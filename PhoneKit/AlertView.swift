b3757a0ca55dbcdab728c30d555b0c81d6bc80d1
Salah <Salahalimohamed1995@gmail.com>
2021-07-05 20:42:11 +0300

    fix bugs

--- PhoneKit/AlertView.swift
+++ PhoneKit/AlertView.swift
@@ -50,8 +50,8 @@ open class AlertView: UIView {
     }
     private func configureXib() {
         self.primaryArray = CountryListManager.shared.getDataFromJSON() ?? []
-//        self.tableView.register(UINib.init(nibName:"AlertCountryTableViewCell", bundle:nil), forCellReuseIdentifier:"AlertCountryTableViewCell");
-        self.tableView.register(AlertCountryTableViewCell.self, forCellReuseIdentifier: "AlertCountryTableViewCell")
+        self.tableView.register(UINib.init(nibName:"AlertCountryTableViewCell", bundle:Bundle.module), forCellReuseIdentifier:"AlertCountryTableViewCell");
+//        self.tableView.register(AlertCountryTableViewCell.self, forCellReuseIdentifier: "AlertCountryTableViewCell")
 
         searchBar.backgroundColor=UIColor.clear;
         searchBar.barTintColor=UIColor.clear;
